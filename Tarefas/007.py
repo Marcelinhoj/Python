e648e8eb627f65fa85dbcd4ff8ba19ddef150ab5
Marcelinhoj <m_seoy@msn.com>
2022-07-25 20:52:43 -0300

    1

---
+++ Tarefas/007.py
@@ -0,0 +1,8 @@
+print('===============================')
+n = int(input('Digite um número : '))
+ante = n - 1 
+suce = n + 1
+print('===============================')
+
+print(' O sucessor do número {} é {} e seu antessessor é {}. '.format(n,suce,ante))
+print('===============================')
