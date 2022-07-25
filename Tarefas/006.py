f562ef1db3ac9a692a6f5c26ad5999aab42738af
Marcelinhoj <m_seoy@msn.com>
2022-07-25 20:57:52 -0300

    1

---
+++ Tarefas/006.py
@@ -0,0 +1,4 @@
+n1 = float(input( 'Digite um número qualquer : '))
+n2 = float(input('Digite outro número qualquer : '))
+soma = n1 + n2
+print(' A soma do número {} mais o número {} é {}'.format(n1, n2, soma))
