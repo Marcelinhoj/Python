45e9925b2aa0120cd996fcef00d6ca3b717b3f0e
Marcelinhoj <m_seoy@msn.com>
2022-07-25 20:54:51 -0300

    1

--- Tarefas/007-media-do-aluno.py
+++
@@ -1,8 +0,0 @@
-print('=======================')
-nome = (input('Qual o seu nome? '))
-n1 = float(input('Digite a primeira nota : '))
-n2 = float(input('Digite sua segunda nota: '))
-soma = n1 + n2
-media = soma / 2
-print('========================================')
-print('{} a soma de suas notas foi {}, vc obteve a media {} .'.format(nome,soma,media))
--- Tarefas/007exercicios-dobro-triplo-raizquadrada.py
+++
@@ -1,7 +0,0 @@
-print('=====================================================')
-n = int(input('Digite uma número qualquer: '))
-dobro = n*2
-triplo = n*3
-raiz = n **(1/2)
-print('=====================================================')
-print(' O dobro do numero {} é {}, \n Seu triplo é {} \n Sua raiz quadrada {:.2f} .. ' .format(n,dobro,triplo,raiz))
